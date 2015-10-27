88088fddf5c512884401d24ad6a30fff187f8811
Fcosta <fcosta@deder.fr>
2015-10-27 19:15:00 +0100

    Application fonctionnel avec gestion des nombre rond et de la suite de nombre de 11 à 16

--- main.js
+++ main.js
@@ -5,7 +5,12 @@
 // tableau des dixaines en lettre
 // tableau des centaines en lettre
 // tableau des rangs
-var tableauUnitees = tableauDixaines = tableauCentaines = [], tableauRang = {};
+var tableauUnitees = [],
+    tableauDixaines = [],
+    tableauCentaines = [],
+    objetRang = {},
+    objetCorrectifNombre = {},
+    number = -1;
 
 tableauUnitees.push("zéro");
 tableauUnitees.push("un");
@@ -40,52 +45,91 @@ tableauCentaines.push("sept-cent");
 tableauCentaines.push("huit-cent");
 tableauCentaines.push("neuf-cent");
 
-tableauRang[1]= tableauUnitees;
-tableauRang[10]=tableauDixaines;
-tableauRang[100]=tableauCentaines;
+objetRang[1] = tableauUnitees;
+objetRang[2] = tableauDixaines;
+objetRang[3] = tableauCentaines;
+
+objetCorrectifNombre["dix-un"] = "onze";
+objetCorrectifNombre["dix-deux"] = "douze";
+objetCorrectifNombre["dix-trois"] = "treize";
+objetCorrectifNombre["dix-quatre"] = "quatorze";
+objetCorrectifNombre["dix-cinq"] = "quinze";
+objetCorrectifNombre["dix-six"] = "seize";
+
+objetCorrectifNombre["dix-zéro"] = tableauDixaines[1];
+objetCorrectifNombre["vingt-zéro"] = tableauDixaines[2];
+objetCorrectifNombre["trente-zéro"] = tableauDixaines[3];
+objetCorrectifNombre["quarante-zéro"] = tableauDixaines[4];
+objetCorrectifNombre["cinquante-zéro"] = tableauDixaines[5];
+objetCorrectifNombre["soixante-zéro"] = tableauDixaines[6];
+objetCorrectifNombre["soixante-dix-zéro"] = tableauDixaines[7];
+objetCorrectifNombre["quatre-vingt-zéro"] = tableauDixaines[8];
+objetCorrectifNombre["quatre-vingt-dix-zéro"] = tableauDixaines[9];
+
+
+//déclaration de le fonction de correction de génération des chiffres en texte
+function remplaceErreurDeLangue(texte, tableau) {
+
+    for (var chaineAtester in tableau) {
+        if (texte.indexOf(chaineAtester) !== -1)
+            texte = texte.replace(chaineAtester, tableau[chaineAtester]);
+    }
+    return texte;
+
+}
 
 // Déclaration de la fonction récupérant un rang Précis
-function recuperationDuRang(nb, nbSelectionRang){
+function recuperationDuRang(nb, nbSelectionRang) {
     //algorithme de recupération de rang
-    var chiffreDuRang = nb % nbSelectionRang;
-    if(typeof chiffreDuRang === "undefined" )
-        chiffreDuRang = 0   
+    var chiffreVersString = nb.toString().split("").reverse();
+    var chiffreDuRangEnString = chiffreVersString[nbSelectionRang - 1];
+    var chiffreDuRang = parseInt(chiffreDuRangEnString);
+    if (isNaN(chiffreDuRang))
+        chiffreDuRang = 0
     return chiffreDuRang;
 }
 
 // Déclaration de la fonction transformant un rang en lettre
-function conversionRangVersTexte(nb, nbSelectionRang){
+function conversionRangVersTexte(nb, nbSelectionRang) {
     //le nombre est transmis à la fonction permettant de récupérant un rang Précis
-    var chiffre = recuperationDuRang(nb,nbSelectionRang);
+    var chiffre = recuperationDuRang(nb, nbSelectionRang);
     //utilisation du tableau en inserant l'index pour récupéré l'item exacte de traduction
-    var chiffreVersLettre = tableauRang[nbSelectionRang][chiffre];
-    //retour de la valeur
+    var chiffreVersLettre = objetRang[nbSelectionRang][chiffre];
+        //retour de la valeur
     return chiffreVersLettre;
 }
-    
 
 
 // déclaration de la fonction exécutant la conversion en lettre
-
+function conversionNombreEnlettre(nb) {
     //Séparation des centaines
     //les centaines sont transmise à la fonction de conversion des centaines
-
+    centainesNumberToText = conversionRangVersTexte(number, 3);
     //Séparation des dizaines
     //les dizaines sont transmise à la fonction de conversion des dizaines
-
+    dizainesNumberToText = conversionRangVersTexte(number, 2);
     //Séparation des unités
     //les unités sont transmise à la fonction de conversion des unités
-
+    unitesNumberToText = conversionRangVersTexte(number, 1);
     // Reconnection des Centaines, dizaines, unités
-
+    var textFinal = "";
+    
+    if (centainesNumberToText === "" && dizainesNumberToText === "" )
+        textFinal = unitesNumberToText;
+    else if (centainesNumberToText === "" )        
+        textFinal = dizainesNumberToText + "-" + unitesNumberToText;
+    else
+        textFinal = centainesNumberToText + "-" + dizainesNumberToText + "-" + unitesNumberToText;
     // renvoie de la valeur
-
+    return remplaceErreurDeLangue(textFinal, objetCorrectifNombre);
+}
 
 /* exécution du programme */
-
-
-// l'utilisateur doit entre un nombre en 0 et 999
-
+while (!isNaN(number)) {
+    // l'utilisateur doit entre un nombre en 0 et 999
+    number = parseInt(prompt('Entrer un nombre entre 0 et 999'));
     // le nombre est transmis à la fonction exécutant la conversion en lettre
-
-    // affichage du nombre en toutes lettres
+    var numberVersText = conversionNombreEnlettre(number)
+        // affichage du nombre en toutes lettres
+    console.log(numberVersText);
+}
