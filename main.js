01636c1a1a35e75a350fe994ea96c11a5688b1b2
Fcosta <fcosta@deder.fr>
2015-10-27 16:43:57 +0100

    Création de la logique algorithmique en commentaire dans le fichier main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+/********************* Algorithmie ***********************/
+
+/* declaration des fonctions et variable */
+// tableau des unitées en lettre
+// tableau des dixaines en lettre
+// tableau des centaines en lettre
+
+
+// Déclaration de la fonction récupérant un rang Précis
+    //algorithme de recupération de rang
+    //retour de la valeur
+
+// Déclaration de la fonction transformant un rang en lettre
+    //le nombre est transmis à la fonction permettant de récupérant un rang Précis
+    //utilisation du tableau en inserant l'index pour récupéré l'item exacte de traduction
+    //retour de la valeur
+    
+
+
+// déclaration de la fonction exécutant la conversion en lettre
+
+    //Séparation des centaines
+    //les centaines sont transmise à la fonction de conversion des centaines
+
+    //Séparation des dizaines
+    //les dizaines sont transmise à la fonction de conversion des dizaines
+
+    //Séparation des unités
+    //les unités sont transmise à la fonction de conversion des unités
+
+    // Reconnection des Centaines, dizaines, unités
+
+    // renvoie de la valeur
+
+
+/* exécution du programme */
+
+
+// l'utilisateur doit entre un nombre en 0 et 999
+
+    // le nombre est transmis à la fonction exécutant la conversion en lettre
+
+    // affichage du nombre en toutes lettres
