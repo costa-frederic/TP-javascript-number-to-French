010bf76a9d0697e024e2be76c55adfc09a0bb258
Fcosta <fcosta@deder.fr>
2015-10-27 17:14:23 +0100

    Ajout du tableau des rang dans l'application javascript et de la fonction de conversion de Rang vers texte

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 // tableau des unitées en lettre
 // tableau des dixaines en lettre
 // tableau des centaines en lettre
-var tableauUnitees = tableauDixaines = tableauCentaines = [];
+// tableau des rangs
+var tableauUnitees = tableauDixaines = tableauCentaines = [], tableauRang = {};
 
 tableauUnitees.push("zéro");
 tableauUnitees.push("un");
@@ -28,17 +29,20 @@ tableauDixaines.push("soixante-dix");
 tableauDixaines.push("quatre-vingt");
 tableauDixaines.push("quatre-vingt-dix");
 
-tableauDixaines.push("");
-tableauDixaines.push("cent");
-tableauDixaines.push("deux-cent");
-tableauDixaines.push("trois-cent");
-tableauDixaines.push("quatre-cent");
-tableauDixaines.push("cinq-cent");
-tableauDixaines.push("six-cent");
-tableauDixaines.push("sept-cent");
-tableauDixaines.push("huit-cent");
-tableauDixaines.push("neuf-cent");
+tableauCentaines.push("");
+tableauCentaines.push("cent");
+tableauCentaines.push("deux-cent");
+tableauCentaines.push("trois-cent");
+tableauCentaines.push("quatre-cent");
+tableauCentaines.push("cinq-cent");
+tableauCentaines.push("six-cent");
+tableauCentaines.push("sept-cent");
+tableauCentaines.push("huit-cent");
+tableauCentaines.push("neuf-cent");
 
+tableauRang[1]= tableauUnitees;
+tableauRang[10]=tableauDixaines;
+tableauRang[100]=tableauCentaines;
 
 // Déclaration de la fonction récupérant un rang Précis
 function recuperationDuRang(nb, nbSelectionRang){
@@ -50,9 +54,14 @@ function recuperationDuRang(nb, nbSelectionRang){
 }
 
 // Déclaration de la fonction transformant un rang en lettre
+function conversionRangVersTexte(nb, nbSelectionRang){
     //le nombre est transmis à la fonction permettant de récupérant un rang Précis
+    var chiffre = recuperationDuRang(nb,nbSelectionRang);
     //utilisation du tableau en inserant l'index pour récupéré l'item exacte de traduction
+    var chiffreVersLettre = tableauRang[nbSelectionRang][chiffre];
     //retour de la valeur
+    return chiffreVersLettre;
+}
     
 
 
